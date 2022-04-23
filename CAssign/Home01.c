cf77a87b4d96e6cf07074931b8bf35c1772f2751
tnwls5621@kpu.ac.kr <tnwls5621@kpu.ac.kr>
2022-04-24 00:08:13 +0900

    숙제완료!

--- CAssign/Home01.c
+++ CAssign/Home01.c
@@ -2,15 +2,17 @@
 
 void main()
 {
-	printf("test");
 	//TOTO : 간단한 자기소개
 	//이름은 변수로
 	//나이도 변수로
 
 	//printf 자기소개
 
-	char name[250] = 신수진;
+	char name[250] = "신수진";
 	int age = 23;
+
+	printf("안녕하세요? 저의 이름은 %s 입니다\n", name);
+	printf("저의 나이는 %d 입니다\n", age);
 }
 
 //새로운 파일을 실행시키고 싶으면 파일에 우클릭하고 '시작프로젝트로 설정'
