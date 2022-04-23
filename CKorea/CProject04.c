2f3b1bc69db273684ba22243a78afe4c6b8f58c8
tnwls5621@kpu.ac.kr <tnwls5621@kpu.ac.kr>
2022-04-23 12:46:55 +0900

    숙제

---
+++ CKorea/CProject04.c
@@ -0,0 +1,37 @@
+//page_32
+
+
+#include <stdio.h>
+
+void main4()
+{
+//1
+	int number1, number2;      // 두 개의 int형 변수를 한 번에 선언
+	number1 = 1;
+	number2 = 2;
+	int number3 = 3, number4 = 4;         // 변수의 선언과 초기화 동시 진행
+
+// 2
+// 가능한 변수명
+	int num = 3;
+	int number5 = 5;
+	int num_ber = 10;
+	int Number = 15;
+
+	//불가능한 변수명
+	//int hello boy = 3;        //공백
+	//int hi#& = 3;              //특수기호
+	//int 8number = 3;        //숫자부터 시작
+	//int short = 3;        //이미 정해져있는 키워드(예약어)
+
+	//3
+
+	int number11 = 3;
+	printf("%d\n", number11);          //3
+	int number22 = 5;
+	printf("%d\n", number22);         //5
+
+
+
+
+}
