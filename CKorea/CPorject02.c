241c47afa03a1e7dad1d31356cbdbd781f8e7146
tnwls5621@kpu.ac.kr <tnwls5621@kpu.ac.kr>
2022-04-23 11:50:44 +0900

    변수와 자료형

---
+++ CKorea/CPorject02.c
@@ -0,0 +1,15 @@
+/*
+  작성자 : 신수진
+  날짜 : 2022-04-23\
+  기능 : printf 사용하기
+*/
+
+
+
+#include <stdio.h>
+
+void main2() {       //main 은 한 프로그램에 하나 만 있을 수 있다
+	printf("작성자 : 신수진");
+	printf("\n날짜 : 2022-04-23");
+	printf("\n기능 : printf사용하기");
+}
